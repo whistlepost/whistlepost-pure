1959c69b5cfe5a3843e3ab6ffbaac278ef0219ed
Ben Fortuna <fortuna@micronode.com>
2023-04-26 18:49:14 +1000

    Added responsive grid support

--- src/main/resources/assets/js/whistlepost.js
+++ src/main/resources/assets/js/whistlepost.js
@@ -3,6 +3,7 @@ import '../css/fonts.css';
 import '../css/style.css';
 import '../css/breadcrumb.css';
 import 'purecss/build/pure-min.css';
+import 'purecss/build/grids-responsive-min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 fontawesome.library.add(solid, regular, brands)
